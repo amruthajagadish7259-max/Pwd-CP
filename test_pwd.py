eb7ae3ad6f118e449af4e883e291c2e8e4015d8b
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2026-01-08 12:07:07 +0530

    Update test_pwd.py

--- test_pwd.py
+++ test_pwd.py
@@ -1,1 +1,16 @@
+from prod import check_password_strength
 
+def test_strong_password():
+    assert check_password_strength("Amrutha123") == "Strong Password"
+
+def test_short_password():
+    assert check_password_strength("Ab12") == "Weak Password"
+
+def test_no_uppercase():
+    assert check_password_strength("amrutha123") == "Weak Password"
+
+def test_no_lowercase():
+    assert check_password_strength("AMRUTHA123") == "Weak Password"
+
+def test_no_digit():
+    assert check_password_strength("Amrutha") == "Weak Password"
